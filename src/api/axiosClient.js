24c8b70b72074bc7dbb83d9ab24db8d72657fe43
PThangJr <pthjrr@gmail.com>
2021-06-19 17:25:56 +0700

    Fix link API in axiosClient

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosClient = axios.create({
-  // baseURL: process.env.REACT_APP_LINK_API || "http://localhost:5000/api/v1",
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: process.env.REACT_APP_LINK_API || "http://localhost:5000/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
